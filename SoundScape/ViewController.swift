2bb428fb7628a6344483880de71c4de68bdfade6
Lauryn <lryndavis@gmail.com>
2017-05-24 12:49:05 -0700

    Initial Commit

---
+++ SoundScape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundScape
+//
+//  Created by Lauryn Davis on 5/24/17.
+//  Copyright © 2017 Lauryn Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
